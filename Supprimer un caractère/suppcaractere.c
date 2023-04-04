701186af30c59468fb31f67927e001b2fa615025
zozio28 <118303280+zozio28@users.noreply.github.com>
2023-04-04 22:11:05 +0200

    Create suppcaractere.c

---
+++ Supprimer un caractère/suppcaractere.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define MAX 100
+
+/* Auteur : Zoé */
+/* Date :  02/03/23 */
+/* Résumé :  Procédure qui supprime le même caractère dans toute a phrase */
+/* Entrée(s) :  ... */
+/* Sortie(s) :  ... */
+void supprCar(char maPhraseInitiale[MAX],char car,int i,int j, char maPhraseModifiee[MAX]){
+  if (maPhraseInitiale[i]!=maPhraseModifiee[j]){
+    i+=1;
+  }
+}
+
+int main(){
+    printf("Création du fichier supprimerCar.c \n");
+    return 0;
+}
