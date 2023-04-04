beb16585479f3442ee9a1ef76fbeaf1b0561c5fc
zozio28 <118303280+zozio28@users.noreply.github.com>
2023-04-04 22:01:49 +0200

    Create EcrireEtoile.c

---
+++ Etoile/EcrireEtoile.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int ecrireetoile(int n)
+{
+    char e [50];
+
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+    
+    {
+        printf("*");
+         ecrireetoile(n+1)
+    }
+    
+    if (n==1)
+    {
+        return e;
+    }
+    else if
+    {
+        if (n==0)
+        {
+            return e;
+        }
+    }
+    else
+    {
+        return(e, ecrireetoile(n+1));
+    }
+}
