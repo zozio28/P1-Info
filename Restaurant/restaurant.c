310f7edcd26ab46a1982f97a393b2152390c4683
zozio28 <zoe.artigas@gmail.com>
2023-04-04 23:15:35 +0200

    zdjp

---
+++ Restaurant/restaurant.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct aliments{
+
+};
+
+void vérifierStock(){
+
+}
+
+int ravitaillerAliment(){
+    char newaliment;
+
+    printf("Entrez l'aliment à ajouter");
+    scanf("%c", newaliment);
+}
+
+int main(){
+    char Stock;
+
+    printf("Veuillez entrer les aliments en Stock");
+    scanf("%s", Stock);
+
+    return 0;
+}
