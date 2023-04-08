590437c97aecaa179bd128b477ec0beec4772f89
zozio28 <zoe.artigas@gmail.com>
2023-04-08 14:23:20 +0200

    refresh

--- Eq 2nd degré/eq2deg.c
+++ Eq 2nd degré/eq2deg.c
@@ -7,15 +7,18 @@ double calculerDelta (double a, double b, double c){
   return pow(b,2)-4*a*c;
 }
 
-//Fonction qui affiche les solutions
+//Fonction qui calcule les solutions
 void solutions(int* nbsolution, double* s1, double* s2, double a, double b, double c){
 
-  if (nbsolution==0){
-    printf("Il y a 1 solution : %lf \n", *s1);
-  }else if (nbsolution>0){
-    printf("Il y a 2 solutions : %lf et %lf \n", *s1, *s2);
+  if (calculerDelta(a,b,c)>=0){
+    *nbsolution = 2;
+    *s1=((-b+sqrt(calculerDelta(a,b,c)))/(2*a));
+    *s2=((-b-sqrt(calculerDelta(a,b,c)))/(2*a));
+  }else if (calculerDelta(a,b,c)==0){
+    *nbsolution = 1;
+    *s1=((-b)/(2*a));
   }else{
-    printf("Il n'y a pas de solution \n");
+    *nbsolution = 0;
   }
 }
 
@@ -26,13 +29,16 @@ int main(){
   double a;
   double b;
   double c;
-  double S1;
-  double S2;
-  int nb;
-  double* s1 = &S1;
-  double* s2 = &S2;
-  int* nbs=&nb;
+  double* s1 = NULL;
+  double* s2 = NULL;
+  int* nbs = NULL;
 
+  //allocation de mémoire
+  s1 = malloc(sizeof(double));
+  s2 = malloc(sizeof(double)); 
+  nbs = malloc(sizeof(int)); 
+
+  //affiche les nombres saisis par l'utilisateur
   printf("Veuillez saisir a : \n");
   scanf("%lf", &a);
   printf("Veuillez saisir b : \n");
@@ -40,20 +46,26 @@ int main(){
   printf("Veuillez saisir c : \n");
   scanf("%lf", &c);
   printf("Vous avez saisi a=> %.1lf b=> %.1lf c=> %.3lf \n\n", a, b, c);
-  printf("Le delta est : %.1lf \n\n", calculerDelta(a, b, c));
-
-  if (calculerDelta(a,b,c)>=0){
-    nb=2;
-    S1=((-b+sqrt(calculerDelta(a,b,c)))/(2*a));
-    S2=((-b-sqrt(calculerDelta(a,b,c)))/(2*a));
-  }else if (calculerDelta(a,b,c)==0){
-    *nbs=1;
-    S1=((-b)/(2*a));
-  }else{
-    *nbs=0;
-  }
+  printf("Le delta est : %.2lf \n\n", calculerDelta(a, b, c));
 
   solutions(nbs,s1,s2,a,b,c);
+
+  //affiche les solutions
+  if (*nbs == 2){
+    printf("Il y a 2 solutions : %lf et %lf \n", *s1, *s2);
+  }else if (*nbs == 1){
+    printf("Il y a 1 solutions : %lf \n", *s1);
+  }else{
+    printf("Il n'y a pas de solution \n");
+  }
+
+  //libère l'espace alloué
+  free(s1);
+  free(s2);
+  free(nbs);
+  s1 = NULL;
+  s2 = NULL;
+  nbs = NULL;
 
   return 0;
 }
