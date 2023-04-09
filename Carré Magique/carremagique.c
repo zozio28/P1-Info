57533fcd94f2a9f38319d42da333423cde95c737
zozio28 <zoe.artigas@gmail.com>
2023-04-09 13:09:57 +0200

    finish

--- Carré Magique/carremagique.c
+++ Carré Magique/carremagique.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Fonction qui permet d'allouer un tableau dynamique
 int** allouerTableau(int nb_lignes,int nb_colonnes){
   int** ptab=NULL;
   ptab=malloc(nb_colonnes*sizeof(int*));
@@ -10,6 +11,7 @@ int** allouerTableau(int nb_lignes,int nb_colonnes){
   return ptab;
 }
 
+//Procédure qui permet d'initialiser un tableau dynamique
 void initTableau(int** tableau,int nb_lignes,int nb_colonnes){
   for (int i=0; i<nb_lignes; i++){
     for (int j=0; j<nb_colonnes; j++){
@@ -18,6 +20,7 @@ void initTableau(int** tableau,int nb_lignes,int nb_colonnes){
   }
 }
 
+//Procédure qui permet d'afficher un tableau dynamique
 void afficherTableau(int** tableau, int nb_lignes, int nb_colonnes){
   for (int i=0; i<nb_colonnes; i++){
     for (int j=0; j<nb_lignes; j++){
@@ -27,81 +30,100 @@ void afficherTableau(int** tableau, int nb_lignes, int nb_colonnes){
   }
 }
 
+//Fonction qui fait la somme d'une ligne donnée
 int cptLigne(int** carre,int ligne,int nb_colonnes){
-  int nb_l;
+  int nb_l=0;
   for (int j=0; j<nb_colonnes; j++){
     nb_l += carre[ligne][j];
   }
-  printf("somme ligne = %d \n", nb_l);
   return nb_l;
 }
 
+//Fonction qui fait la somme d'une colonne donnée
 int cptColonne(int** carre,int colonne,int nb_lignes){
   int nb_c=0;
   for (int j=0; j<nb_lignes; j++){
     nb_c += carre[j][colonne];
   }
-  printf("somme colonne = %d \n", nb_c);
   return nb_c;
 }
 
+//Fonction qui fait la somme de la diagonale (droite-gauche)
 int cptDiag1(int** carre,int nb_lignes,int nb_colonnes){
   int nb_d1=0;
   for (int i=0; i<nb_colonnes; i++){
     nb_d1 += carre[i][nb_colonnes-1-i];
   }
-  printf("somme diagonale 1 = %d \n", nb_d1);
   return nb_d1;
 }
 
+//Fonction qui fait la somme de la diagonale (gauche-droite)
 int cptDiag2(int** carre,int nb_lignes,int nb_colonnes){
   int nb_d2=0;
   for (int i=0; i<nb_colonnes; i++){
     nb_d2 += carre [i][i];
   }
-  printf("sommediagonale 2 = %d \n", nb_d2);
   return nb_d2;
 }
 
+//Prédicat qui permet de savoir si un carré est magique
 int estMagique(int** carre,int nb_lignes,int nb_colonnes){
-  int ref = cptDiag1(carre,nb_lignes,nb_colonnes);
-  int res = cptDiag2(carre,nb_lignes,nb_colonnes);
-  int i;
-  int n=3;
+  int sommemagique = nb_lignes*(nb_lignes*nb_colonnes +1)/2;
 
-  if (ref == res){
-    return i = 0;
-  }else{
-    return i=n+1;
+  if(cptDiag1(carre,nb_lignes,nb_colonnes)!=sommemagique || cptDiag2(carre,nb_lignes,nb_colonnes)!=sommemagique){
+    return 0;
   }
-  while ((i<n) && (cptLigne(carre,nb_lignes,nb_colonnes)==ref)){
-    i=i+1;
+  for (int i=0;i<nb_lignes;i++){
+    if (cptLigne(carre,i,nb_colonnes)!=sommemagique || cptColonne(carre,i,nb_lignes)!=sommemagique){
+      return 0;
+    }
   }
-
+  return 1;
 }
 
+//Fonction principale
 int main(){
+
+  //Variables
   int** tab;
   int lignes=3;
   int colonnes=3;
 
+  //allocation de mémoire
   tab=allouerTableau(lignes,colonnes);
+
+  //initialisation du tableau dynamique
   initTableau(tab,lignes,colonnes);
 
+  //demande à l'utilisateur la saisie des valeurs du carré magique
   for (int i=0; i<lignes; i++){
     for (int j=0; j<colonnes; j++){
       printf("l=%d et c=%d saisir la valeur : \n", i,j);
       scanf("%d", &tab[i][j]);
     }
   }
+  printf("\n");
 
+  //affichage du carré magique
   afficherTableau(tab,lignes,colonnes);
 
-  cptLigne(tab,lignes,colonnes);
-  cptColonne(tab,colonnes,lignes);
-  cptDiag1(tab,lignes,colonnes);
-  cptDiag2(tab,lignes,colonnes);
+  //affiche la somme des colonnes,lignes,diagonales
+  printf("\n");
+  for(int i=0 ; i<lignes ; i++){
+    printf("somme ligne n°%d = %d \n", i, cptLigne(tab,i,colonnes));
+    printf("somme colonne n°%d = %d \n", i, cptColonne(tab,i,lignes));
+  }
+  printf("somme diagonale n°1 = %d \n", cptDiag1(tab,lignes,colonnes));
+  printf("somme diagonale n°2 = %d \n", cptDiag2(tab,lignes,colonnes));
 
+  //affiche si le carré est magique
+  if (estMagique(tab,lignes,colonnes)){
+    printf("\nLe carré est magique \n");
+  }else{
+    printf("\nLe carré n'est pas magique \n");
+  }
+
+  //libère l'espace alloué
   free(tab);
   tab=NULL;
 
